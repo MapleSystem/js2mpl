6119cb69ec0bf46a03416eea069a0a57ad3e3a80
HU Wen <hu.wen@huawei.com>
2015-11-10 10:53:54 -0800

    add regression-tests.pl

--- tests/regression-tests.pl
+++ tests/regression-tests.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-my $tempdir = "./todo-tests/temp";
+my $tempdir = "./regression-tests/temp";
 if(!(-e $tempdir)) {
   mkdir $tempdir;
 }
@@ -8,16 +8,16 @@ my @failed_mpl_file;
 my @failed_mmpl_file;
 my @failed_int_file;
 my @successed_file;
-$dirname = "./todo-tests";
+$dirname = "./regression-tests";
 chdir $dirname;
-#$dirname = "./";
-#opendir ( DIR, $dirname ) || die "Error in opening dir $dirname\n";
-#print("\n============= regression tests ================\n");
-#while( ($filename = readdir(DIR))){
-#  if(-d $filename and $filename ne "." and $filename ne "..") {
-#    use Cwd;
-#    my $predir = getcwd;
-#    chdir $filename;
+$dirname = "./";
+opendir ( DIR, $dirname ) || die "Error in opening dir $dirname\n";
+print("\n============= regression tests ================\n");
+while( ($filename = readdir(DIR))){
+  if(-d $filename and $filename ne "." and $filename ne ".." and $filename ne "temp") {
+    use Cwd;
+    my $predir = getcwd;
+    chdir $filename;
     my @files = <*.js>;
 
     foreach $fullname (@files) {
@@ -30,20 +30,20 @@ chdir $dirname;
       my $log_file = $file.'.log';
       #my $option = $ARGV[0];
       my $option = 0;
-      system("cp $js_file ./temp/$js_file");
-      my $res = system("../../build/js2mpl ./temp/$js_file $option > ./temp/$log_file");
+      system("cp $js_file ../temp/$js_file");
+      my $res = system("../../../build/js2mpl ../temp/$js_file $option > ../temp/$log_file");
       if ($res > 0) {
         push(@failed_mpl_file, $file);
         $flag ++;
         next;
       }
-      $res = system("../../../mapleall/maplebe/build/be/mplbe ./temp/$mpl_file >> ./temp/$log_file");
+      $res = system("../../../../mapleall/maplebe/build/be/mplbe ../temp/$mpl_file >> ../temp/$log_file");
       if ($res > 0) {
         push(@failed_mmpl_file, $file);
         $flag ++;
         next;
       }
-      $res = system("../../../mapleall/maplevm/build/interpreter32 ./temp/$mmpl_file >> ./temp/$log_file");
+      $res = system("../../../../mapleall/maplevm/build/interpreter32 ../temp/$mmpl_file >> ../temp/$log_file");
       if ($res > 0) {
         push(@failed_int_file, $file);
         $flag ++;
@@ -51,17 +51,17 @@ chdir $dirname;
       }
       if ($flag eq 0) {
         push(@successed_file, $file);
-        system("rm -f ./temp/$js_file");
-        system("rm -f ./temp/$mpl_file");
-        system("rm -f ./temp/$mmpl_file");
-        system("rm -f ./temp/$log_file");
+        system("rm -f ../temp/$js_file");
+        system("rm -f ../temp/$mpl_file");
+        system("rm -f ../temp/$mmpl_file");
+        system("rm -f ../temp/$log_file");
         next;
       }
     }
-#    chdir $predir;
-#  }
-#}
-#closedir(DIR);
+    chdir $predir;
+  }
+}
+closedir(DIR);
 
 if ((scalar(@failed_mpl_file) + scalar(@failed_mmpl_file) + scalar(@failed_int_file)) eq 0) {
   print("\n all ok\n");
