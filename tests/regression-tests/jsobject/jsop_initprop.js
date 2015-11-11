61655e95ca22322a85c6d377983846821b8c6c04
yueming.yang <yueming.yang@huawei.com>
2015-11-12 02:10:31 +0800

    pass three object-cases

---
+++ tests/regression-tests/jsobject/jsop_initprop.js
@@ -0,0 +1,10 @@
+
+var obj={firstname:"John"};
+if (obj.firstname !== "John")
+  $ERROR("initprop failed: expect obj.firstname == John but get", obj.firstname);
+//if (obj.lastname !== "Doe")
+  //$ERROR("initprop failed: expect obj.lastname == Doe but get", obj.lastname);
+//if (obj.age !== 50)
+//  $ERROR("initprop failed: expect obj.age == 50 but get", obj.age);
+//if (obj.eyecolor !== "blue")
+//  $ERROR("initprop failed: expect obj.eyecolor == blue but get", obj.eyecolor);
