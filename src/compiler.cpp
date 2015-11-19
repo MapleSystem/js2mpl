2e7dd76833edad98bfef0b0b77025d6400668306
Fred Chow <fred.chow@huawei.com>
2015-11-18 20:35:56 -0800

    workaround in case local_no does not correspond to a user variable

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -711,6 +711,8 @@ void JSCompiler::CompileOpSetArg(uint32_t i, BaseNode *val) {
 BaseNode *JSCompiler::CompileOpGetLocal(uint32_t local_no) {
   JSMIRFunction *func = jsbuilder_->GetCurrentFunction();
   char *name = closure_->GetLocalVar(func, local_no);
+  if (name == NULL)
+    name = Util::GetSequentialName("local_var_", local_no, mp_);
   MIRSymbol *var = jsbuilder_->GetOrCreateLocalDecl(name, jsvalue_type_);
 #if 1
   return jsbuilder_->CreateExprDread(jsvalue_type_, var);
@@ -726,6 +728,8 @@ BaseNode *JSCompiler::CompileOpGetLocal(uint32_t local_no) {
 BaseNode *JSCompiler::CompileOpSetLocal(uint32_t local_no, BaseNode *src) {
   JSMIRFunction *func = jsbuilder_->GetCurrentFunction();
   char *name = closure_->GetLocalVar(func, local_no);
+  if (name == NULL)
+    name = Util::GetSequentialName("local_var_", local_no, mp_);
   uint32_t curtag = DetermineTagFromNode(src);
   MIRSymbol *var = jsbuilder_->GetOrCreateLocalDecl(name, jsvalue_type_);
 
