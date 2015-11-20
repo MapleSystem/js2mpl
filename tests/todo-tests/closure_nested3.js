ddc66257746dd8213b1edfea3a5a9c79114057a3
HU Wen <hu.wen@huawei.com>
2015-11-20 13:45:57 -0800

    adjust closure_nested3.js to test the intended issue

--- tests/todo-tests/closure_nested3.js
+++ tests/todo-tests/closure_nested3.js
@@ -4,14 +4,11 @@ var innermostbar;
 function bar() {
   var counter = 10;
   innerbar = function bar1() {
-    counter ++;
-    var pp = counter;
+    var counter = 250;
     innermostbar = function bar2() {
       counter ++;
-      pp ++;
       return counter;
     }
-    var counter = 250;
     return counter;
   }
   innerbar2 = function bar12() {
@@ -30,3 +27,4 @@ if (a!=10) $ERROR("failed: expect 10 but get", a);
 if (b!=250) $ERROR("failed: expect 250 but get", b);
 if (c!=251) $ERROR("failed: expect 251 but get", c);
 if (d!=11) $ERROR("failed: expect 11 but get", d);
+print("pass");
