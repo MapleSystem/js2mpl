4f74f5563ad398868ca16acfb1db8739e27546d5
Zhang Hui <zhanghui2@huawei.com>
2016-03-14 16:07:54 +0800

    JSON tests

--- tests/regression-tests/samples/obj-iter2.js
+++ tests/regression-tests/samples/obj-iter2.js
@@ -7,6 +7,6 @@ foo[99] = 999;
 var result = "";
 for (var i in foo)
   result += i;
-if (result === "99geef1")
+if (result === "99f1gee")
   print("pass\n");
 else $ERROR("test failed: expect f1gee99 but get ", result);
