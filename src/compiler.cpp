271007bea6e9a182fdcc51687b77204a752c6ba8
Ed Ching <eching@futurewei.com>
2021-04-03 14:51:27 -0700

    Add copyright message. 	modified:   include/builtinmethod.def 	modified:   include/closure.h 	modified:   include/compiler.h 	modified:   include/eh.h 	modified:   include/js2mpl.h 	modified:   include/jsbuiltinstrings.inc.h 	modified:   include/jsfunction.h 	modified:   include/jsmirbuilder.h 	modified:   include/jsvalue.h 	modified:   include/macros.h 	modified:   include/operandstack.h 	modified:   include/opstack.h 	modified:   include/scope.h 	modified:   include/util.h 	modified:   src/Makefile 	modified:   src/closure.cpp 	modified:   src/compiler.cpp 	modified:   src/eh.cpp 	modified:   src/js2mpl.cpp 	modified:   src/jsfunction.cpp 	modified:   src/jsmirbuilder.cpp 	modified:   src/opcname.def 	modified:   src/operandstack.cpp 	modified:   src/scope.cpp 	modified:   src/util.cpp

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,9 +1,22 @@
-/// Copyright [year] <Copyright Owner>
+/*
+ * Copyright (C) [2021] Futurewei Technologies, Inc. All rights reserved.
+ *
+ * OpenArkCompiler is licensed under the Mulan Permissive Software License v2.
+ * You can use this software according to the terms and conditions of the MulanPSL - 2.0.
+ * You may obtain a copy of MulanPSL - 2.0 at:
+ *
+ *   https://opensource.org/licenses/MulanPSL-2.0
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+ * FIT FOR A PARTICULAR PURPOSE.
+ * See the MulanPSL - 2.0 for more details.
+ */
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <vector>
-#include <cassert>
+#include <assert.h>
 #include <locale>
 #include <codecvt>
 #include "js/src/jsscript.h"
