4a9696e4e6398dfc480f1c3021c44e275f1d86d2
Liya Chen <lchen@futurewei.com>
2021-04-03 15:02:23 -0700

    add copyright info

--- include/opstack.h
+++ include/opstack.h
@@ -1,7 +1,20 @@
-/// Copyright [year] <Copyright Owner>
+/*
+ * Copyright (C) [2021] Futurewei Technologies, Inc. All rights reserved.
+ *
+ * OpenArkCompiler is licensed under the Mulan Permissive Software License v2.
+ * You can use this software according to the terms and conditions of the MulanPSL - 2.0.
+ * You may obtain a copy of MulanPSL - 2.0 at:
+ *
+ *   https://opensource.org/licenses/MulanPSL-2.0
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+ * FIT FOR A PARTICULAR PURPOSE.
+ * See the MulanPSL - 2.0 for more details.
+ */
 #ifndef JS2MPL_INCLUDE_OPSTACK_H_
 #define JS2MPL_INCLUDE_OPSTACK_H_
-#include <cassert>
+#include <assert.h>
 #include <vector>
 #include "maple_ir/include/mir_builder.h"
 #include "util.h"
